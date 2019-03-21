70be2daab1596544626a6ce62153a2472f81cd66
Julia Bresolin <juliabresolin2@gmail.com>
2019-03-20 21:35:49 -0300

    ajustes css navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InformativoComponent } from './informativo/informativo.component';
 import { JavaClassComponent } from './java-class/java-class.component';
 import { InformativoDoisComponent } from './informativo-dois/informativo-dois.component';
 import { NavComponent } from './nav/nav.component';
+import {MatDivider} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +17,14 @@ import { NavComponent } from './nav/nav.component';
     InformativoComponent,
     JavaClassComponent,
     InformativoDoisComponent,
-    NavComponent
+    NavComponent,
+    MatDivider
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
